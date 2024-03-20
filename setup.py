57fb741bdc2093702a84526e6417e28f839f057d
waterthree <waterthree@gmail.com>
2024-03-20 13:46:44 -0400

    switch to gazebo package

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'g7_world'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', 'custom_world.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'walker = g7_world.CPSC_5207EL-02_A3_G7:main',
+     #       'walker = g7_world.CPSC_5207EL-02_A3_G7:main',
         ],
     },
 )
